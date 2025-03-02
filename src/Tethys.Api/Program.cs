d6e1727e528a33ffc03f19de0d5187df3e4ba7f7
Daryl Walleck <dwalleck@gmail.com>
2025-03-02 12:12:21 -0600

    Updated package versions

--- src/Tethys.Api/Program.cs
+++ src/Tethys.Api/Program.cs
@@ -15,7 +15,6 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
 
-
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
